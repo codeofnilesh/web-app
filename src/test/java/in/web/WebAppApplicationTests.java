acc75855164df83bf7efa71859e3512d98e3f2f1
Nilesh <nileshphatak4@gmail.com>
2024-07-02 17:35:58 +0530

    first commit

---
+++ src/test/java/in/web/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
